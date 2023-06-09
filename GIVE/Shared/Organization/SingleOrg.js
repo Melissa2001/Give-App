8800b645b0deb61ba17f1fe6faaaf4f2cbe85303
Melissa2001 <melissamanojthondoli2024@cs.sjcetpalai.ac.in>
2023-06-10 01:39:45 +0530

    org image set

--- GIVE/Shared/Organization/SingleOrg.js
+++ GIVE/Shared/Organization/SingleOrg.js
@@ -1,44 +1,37 @@
 import React,{useState,useEffect} from "react";
-import {Image,View,StyleSheet,Text,ScrollView,Button} from 'react-native';
+import {Image,View,StyleSheet,Text,ScrollView,Button,Dimensions} from 'react-native';
 import {Left,Right,Container,H1} from 'native-base';
-
+var { width } = Dimensions.get("window")
 const SingleOrg=(props)=>{
 
     const [item,setItem] = useState(props.route.params.item);
     const [availability,setAvailability]=useState('');
 
     return(
-        <Container style={styles.container}>
+        
             <ScrollView style={{marginBottom:80,padding:5}}>
-                <View>
+                
+                    
                     <Image
                         source={{
-                            uri:item.Image?item.Image:'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                            uri:item.image?item.image:'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
                         }}
-                    resizeMode="contain"
-                    style={styles.image}
+                        resizeMode="cover"
+                        style={styles.image}
                     />
-                </View>
+                
 
             </ScrollView>
-s
-        </Container>
+       
     )
 }
 
 const styles=StyleSheet.create({
-    container:{
-        position:'relative',
-        height:'100%'
-    },
-    imageContainer:{
-        backgroundColor:'white',
-        padding:0,
-        margin:0
-    },
     image:{
-        width:'100%',
-        height:250
+        height: width,
+        width: width ,
+        borderRadius:20,
+        alignSelf:'center'
     }
 })
 
