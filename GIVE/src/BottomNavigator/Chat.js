1787e24433c995af56056b13b56f43b2c26af5bb
Dona Siby <dona.c.siby@gmail.com>
2023-06-23 23:48:20 +0530

    Chat page 80 percent completed

--- GIVE/src/BottomNavigator/Chat.js
+++ GIVE/src/BottomNavigator/Chat.js
@@ -1,13 +1,106 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, Button, Image, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Card,
+  UserInfo,
+  UserImgWrapper,
+  UserImg,
+  UserInfoText,
+  UserName,
+  PostTime,
+  MessageText,
+  TextSection,
+} from '../../assets/data/messageStyles'
+
+
 import TopBar from '../../Shared/Organization/TopBar';
+
+const Messages = [{
+  id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+  fullName: "Aafreen Khan",
+  timeStamp: "12:47 PM",
+  recentText: "Good Day!",
+  avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+}, {
+  id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+  fullName: "Sujitha Mathur",
+  timeStamp: "11:11 PM",
+  recentText: "Cheer up, there!",
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU"
+}, {
+  id: "58694a0f-3da1-471f-bd96-145571e29d72",
+  fullName: "Anci Barroco",
+  timeStamp: "6:22 PM",
+  recentText: "Good Day!",
+  avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg"
+}, {
+  id: "68694a0f-3da1-431f-bd56-142371e29d72",
+  fullName: "Aniket Kumar",
+  timeStamp: "8:56 PM",
+  recentText: "All the best",
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+}, {
+  id: "28694a0f-3da1-471f-bd96-142456e29d72",
+  fullName: "Kiara",
+  timeStamp: "12:47 PM",
+  recentText: "I will call today.",
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+}];
 
 
 const Chat = () => {
-    return (
-      <TopBar/>
-        
-      );
-    }
+  const navigation = useNavigation();
+   
+  return (
+    <View style={styles.container}>
+      <TopBar />
+      <Container>
+      <FlatList
+      data={Messages}
+      keyExtractor={item=>item.id}
+      renderItem={({item})=>(
+        <Card onPress={()=>navigation.navigate('ChatScreen',{fullName:item.fullName})}>
+          <UserInfo>
+            <UserImgWrapper>
+              <Image source={{ uri: item.avatarUrl }} style={styles.userImg} />
+            </UserImgWrapper>
+            <TextSection>
+              <UserInfoText>
+                <UserName>{item.fullName}</UserName>
+                <PostTime>{item.timeStamp}</PostTime>
+              </UserInfoText>
+              <MessageText>{item.recentText}</MessageText>
 
-export default Chat
+            </TextSection>
+          </UserInfo>
+        </Card>
+      )
+    }
+    />
+    </Container>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    userImg: {
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+    },
+  });
+
+
+export default Chat;
