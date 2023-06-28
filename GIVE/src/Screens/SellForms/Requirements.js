65d41a92617b182cb48dd6305c1fe976d74086fc
KARUN CHERY_JAMES <karunchery@gmail.com>
2023-06-28 23:33:39 +0530

    modified:   GIVE/src/Screens/SellForms/Requirements.js 	modified:   backend/routes/requirements.js

--- GIVE/src/Screens/SellForms/Requirements.js
+++ GIVE/src/Screens/SellForms/Requirements.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Pressable, HStack, Badge, Spacer, ScrollView } from 'native-base';
 import CommonButton from '../../../Shared/Form/CommonButton';
 import { useNavigation } from '@react-navigation/native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import AddPostScreen from '../ChatAndGratitude/AddPostScreen';
-const handlePress = () => {
-  console.log('Button pressed!');
-};
+import axios from 'axios';
+import baseURL from '../../../assets/common/baseUrl';
 
 const Requirement = () => {
   const navigation = useNavigation();
+  const [requirements, setRequirements] = useState([]);
+
+  useEffect(() => {
+    fetchRequirements();
+  }, []);
+
+  const fetchRequirements = async () => {
+    try {
+      const response = await axios.get(`${baseURL}requirements`);
+      if (response.status === 200) {
+        setRequirements(response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching requirements:', error);
+    }
+  };
+
+  const handlePress = () => {
+    console.log('Button pressed!');
+  };
 
   return (
-    <ScrollView style={{ backgroundColor: "#fff" }}>
+    <ScrollView style={{ backgroundColor: '#fff' }}>
       <View style={{ marginRight: 10, alignSelf: 'flex-end' }}>
         <FontAwesome5.Button
           name="plus"
@@ -23,23 +41,16 @@ const Requirement = () => {
         />
       </View>
 
-      <View style={{ marginTop: 40 }}>
-        <RequirementCard />
-      </View>
-      <View style={{ marginTop: 40 }}>
-        <RequirementCard />
-      </View>
-      <View style={{ marginTop: 40 }}>
-        <RequirementCard />
-      </View>
-      <View style={{ marginTop: 40, marginBottom: 40 }}>
-        <RequirementCard />
-      </View>
+      {requirements.map((requirement, index) => (
+        <View key={index} style={{ marginTop: 40 }}>
+          <RequirementCard requirement={requirement} handlePress={handlePress} />
+        </View>
+      ))}
     </ScrollView>
   );
 };
 
-function RequirementCard() {
+function RequirementCard({ requirement, handlePress }) {
   return (
     <View alignItems="center">
       <Pressable maxW={96}>
@@ -55,18 +66,18 @@ function RequirementCard() {
           >
             <HStack alignItems="center">
               <Badge colorScheme="purple" _text={{ color: 'white' }} variant="solid" rounded={4}>
-                1 requirement
+                {requirement.requirementCount} requirement
               </Badge>
               <Spacer />
               <Text fontSize={10} color="coolGray.800">
-                Posted on 28/06/2023
+                Posted on {requirement.postedDate}
               </Text>
             </HStack>
             <Text color="coolGray.800" mt={3} fontWeight="medium" fontSize="xl">
-              Maryasadanm
+              {requirement.organizationName}
             </Text>
             <Text mt={2} fontSize="sm" color="coolGray.700">
-              Unlock powerful time-saving tools for creating email delivery and collecting marketing data
+              {requirement.requirement}
             </Text>
             <CommonButton title={'Message'} textColor={'#4caf50'} onPress={handlePress} />
           </View>
