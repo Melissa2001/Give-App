71b7f0d5199f83079f892cc7f1dd9b500af2ec54
GAUTHAM BABU <83872204+PenguinXD03@users.noreply.github.com>
2023-06-30 08:28:04 +0530

    Create post.js

---
+++ backend/routes/post.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Route: /api/posts
+// Method: POST
+// Description: Create a new post
+router.post('/', async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    // Create a new post object
+    const newPost = new Post({
+      title,
+      content
+    });
+
+    // Save the post to the database
+    const savedPost = await newPost.save();
+
+    res.status(201).json(savedPost);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create the post' });
+  }
+});
+
+module.exports = router;
